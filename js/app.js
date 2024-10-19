2e81f9bee320978489fb5506a5a9e21e3ce628f6
JohnVitor-DEV <johnvitoralves39@gmail.com>
2024-10-19 06:42:51 -0300

    Refactor CSS files and add color theme functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
-const mainIllustration = document.querySelector('.main-illustration');
 const loginRegisterOptions = document.querySelector('.loginRegisterOptions');
 const loginPage = document.querySelector('.loginPage');
 const registerPage = document.querySelector('.registerPage');
+const mainIllustration = document.querySelector('.main-illustration');
+const secondaryIllustration = document.querySelector('.secondary-illustration');
+const mainIllustrationLogo = mainIllustration.querySelector('img');
+
+let boolLight = false;
 
 function showLoginPage() {
   loginRegisterOptions.classList.add('hidden');
@@ -22,4 +26,23 @@ function backToMain() {
   registerPage.classList.add('hidden');
   mainIllustration.classList.remove('hidden');
   loginRegisterOptions.classList.remove('hidden');
+}
+
+function colorTheme() {
+  if (boolLight) {
+    mainIllustrationLogo.src = "../assets/svg/undraw-veterinaryClinic.svg";
+    document.documentElement.style.setProperty('--lamp', 'url(../assets/svg/lamp-light-off.svg)');
+    document.documentElement.style.setProperty('--duskTheme', '#222222');
+    document.documentElement.style.setProperty('--text', '#ffffff');
+    boolLight = false;
+    console.log("boolLight = false (desligado)");
+  } else {
+    mainIllustrationLogo.src = "../assets/svg/undraw-veterinaryClinic-light.svg";
+    document.documentElement.style.setProperty('--lamp', 'url(../assets/svg/lamp-light-on.svg)');
+    document.documentElement.style.setProperty('--duskTheme', '#ffffff');
+    document.documentElement.style.setProperty('--text', '#000000');
+    boolLight = true;
+    console.log("boolLight = true (ligado)");
+  }
+
 }
