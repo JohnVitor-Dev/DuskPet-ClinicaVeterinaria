bbf5a117f49f4aaab78a651e2f81cff60b36cd66
JohnVitor-Dev <johnvitoralves39@gmail.com>
2025-08-17 05:45:50 -0300

    Add background image and restructure HomePage layout for better visual appeal

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router-dom";
 
 import TopBar from "../components/TopBar.jsx";
 
+import HomeBackground from "../assets/images/home-background.png"
+
 export default function HomePage() {
     const token = localStorage.getItem("token");
     const navigate = useNavigate();
@@ -15,16 +17,19 @@ export default function HomePage() {
         <div className="home-container">
             <TopBar />
             <div className="home-frame-main">
-                <div className="home-main">
-                    <h2>Bem-vindo à DuskPet</h2>
-                    <p> A DuskPet é uma clínica veterinária especializada no cuidado com seu pet.
-                        Nosso sistema foi desenvolvido para facilitar o agendamento de consultas,
-                        acesso aos dados dos animais e controle eficiente de medicamentos.</p>
+                <div className="home-content">
+                    <div className="home-main">
+                        <h2>Bem-vindo à DuskPet</h2>
+                        <p> A DuskPet é uma clínica veterinária especializada no cuidado com seu pet.
+                            Nosso sistema foi desenvolvido para facilitar o agendamento de consultas,
+                            acesso aos dados dos animais e controle eficiente de medicamentos.</p>
+                    </div>
+                    <div className="home-main-buttons">
+                        <button onClick={() => navigate("/register")}>Registrar</button>
+                        <p>Já possui uma conta? <a onClick={() => navigate("/login")}>Login</a></p>
+                    </div>
                 </div>
-                <div className="home-main-buttons">
-                    <button onClick={() => navigate("/register")}>Registrar</button>
-                    <p>Já possui uma conta? <a onClick={() => navigate("/login")}>Login</a></p>
-                </div>
+                <div className="home-background"> <img src={HomeBackground} alt="Home Background" /></div>
             </div>
         </div>
     );
